acb7a13d00f53e13cbf79aefdcdd5314cbfb83c0
iharshgarg04 <iharshgarg04@gmail.com>
2024-07-15 17:32:43 +0530

    server setup

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors({
+    origin:"*",
+}))
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+const connectDb = async()=>{
+    try{
+        const response = await mongoose.connect(`mongodb+srv://iharshgarg04:${process.env.PASSWORD}@taskmanagement.shzry1k.mongodb.net/?retryWrites=true&w=majority&appName=taskManagement`);
+        if(response){
+            console.log("Database connected Successfully");
+        }
+    }catch(error){
+        console.log("Error while connecting to mongoDb");
+    }
+}
+connectDb();
+
+app.listen(PORT,()=>{
+    console.log(`server is listening on PORT: `,PORT);
+})
