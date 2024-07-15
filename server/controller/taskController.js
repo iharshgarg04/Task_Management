9b066d05771407d44bfb00d3d7c5dc57fd0f2371
iharshgarg04 <iharshgarg04@gmail.com>
2024-07-15 18:55:17 +0530

    edit task api

--- server/controller/taskController.js
+++ server/controller/taskController.js
@@ -30,3 +30,34 @@ exports.addTask = async(req,res)=>{
         console.log("Error while adding task");
     }
 }
+
+exports.editTask = async(req,res)=>{
+    try{    
+        const {title,description,dueDate,priority} = req.body;
+        if(!title || !description || !dueDate || !priority){
+            return res.status(400).json({
+                success:false,
+                message:"All fields required",
+            })
+        }
+        const task = await Task.findByIdAndUpdate(req.params.id,{
+            title,
+            description,
+            dueDate,
+            priority,
+        })
+        if(!task){
+            return res.status(400).json({
+                success:true,
+                message:"Task not found",
+            })
+        }
+        return res.status(200).json({
+            success:true,
+            message:"Task is updated successfully",
+        })
+    }catch(error){
+        console.log(error);
+        console.log("error while editing task");
+    }
+}
--- server/routes/taskRoute.js
+++ server/routes/taskRoute.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { addTask } = require('../controller/taskController');
+const { addTask, editTask } = require('../controller/taskController');
 const router = express.Router();
 
 router.post('/addTask',addTask);
+router.post('/editTask/:id',editTask);
 
 module.exports = router;
