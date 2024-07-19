3b5db2a3f5d3de71c24cb6a66c788745b47d3d76
iharshgarg04 <iharshgarg04@gmail.com>
2024-07-20 02:48:40 +0530

    added logout functionality

--- client/src/app/mycomponents/navbar/navbar.component.ts
+++ client/src/app/mycomponents/navbar/navbar.component.ts
@@ -3,35 +3,47 @@ import { MenuItem } from 'primeng/api';
 import { MenuModule } from 'primeng/menu';
 import { ButtonModule } from 'primeng/button';
 import { Router } from '@angular/router';
-
+import { CommonModule } from '@angular/common';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MenuModule, ButtonModule,],
+  imports: [MenuModule, ButtonModule, CommonModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
   items: MenuItem[] | undefined;
-  constructor(private router :Router){}
+  private userIdSubject = new BehaviorSubject<boolean>(!!localStorage.getItem('userData'));
+  userId$ = this.userIdSubject.asObservable();
+  constructor(private router: Router) {}
+
+  logout(): void {
+    localStorage.removeItem('userData');
+    this.userIdSubject.next(true);
+    this.router.navigate(['/']);
+  }
+
   ngOnInit() {
-      this.items = [
+    const users = localStorage.getItem('userData'); 
+
+    this.items = [
+      {
+        label: 'Options',
+        items: [
           {
-              label: 'Options',
-              items: [
-                  {
-                      label: 'History',
-                      icon: 'pi pi-history',
-                      routerLink:['/history'],
-                  },
-                  {
-                      label: 'Home',
-                      icon: 'pi pi-home',
-                      routerLink:['/home'],
-                  }
-              ]
-          }
-      ];
+            label: 'History',
+            icon: 'pi pi-history',
+            routerLink: ['/history'],
+          },
+          {
+            label: 'Home',
+            icon: 'pi pi-home',
+            routerLink: ['/home'],
+          },
+        ],
+      },
+    ];
   }
 }
